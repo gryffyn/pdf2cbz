09b59954f28bf89cbcb1b7af44647821eef2885e
gryffyn <me@neveris.one>
2021-10-13 01:11:04 +0100

    added crop feature (good luck)
    Signed-off-by: gryffyn <me@neveris.one>

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # pdf2cbz
 Converts PDF files to CBZ archives.
 
-### A Word of Warning
-Don't use the PNG option as it is right now, it tends to increase the size of the CBZ relative to the PDF by about 10x. So a 58MB PDF outputs a CBZ around 530MB.
+### A Word(s) of Warning
+- Don't use the PNG option as it is right now, it tends to increase the size of the CBZ relative to the PDF by about 2-10x. So a 58MB PDF outputs a CBZ around 530MB.
+- the crop feature is quite limited at the minute, it's like 1 am and my brain can't handle doing the math for percentage right now, so you'll just have to guess for the pixel dimensions to crop off. I'll fix this soon, moving to percentage off each side instead of px.
 
 ## Building
 Requires CGO.
@@ -24,9 +25,9 @@ Usage:
 Application Options:
   -i, --images        Extracts images from PDF (default: extracts pages)
   -p, --png           Outputs pages as PNG (default: jpeg)
+  -c, --crop=         Dimensions of the crop region (css shorthand, comma-delimited)
   -q, --quality=      JPEG quality (0-100) (default: 85)
 
 Help Options:
   -h, --help          Show this help message
-  
 ```
--- pdf2cbz.go
+++ pdf2cbz.go
@@ -3,11 +3,14 @@ package main
 import (
 	"archive/zip"
 	"fmt"
+	"image"
 	"image/jpeg"
 	"image/png"
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/gen2brain/go-fitz"
 	"github.com/jessevdk/go-flags"
@@ -18,10 +21,11 @@ import (
 
 func main() {
 	type Opts struct {
-		Images      bool `short:"i" long:"images" description:"Extracts images from PDF (default: extracts pages)"`
-		PNG         bool `short:"p" long:"png" description:"Outputs pages as PNG (default: jpeg)"`
-		JPEGQuality int  `short:"q" long:"quality" description:"JPEG quality (0-100)" default:"85"`
-		Positional  struct {
+		Images         bool   `short:"i" long:"images" description:"Extracts images from PDF (default: extracts pages)"`
+		PNG            bool   `short:"p" long:"png" description:"Outputs pages as PNG (default: jpeg)"`
+		CropDimensions string `short:"c" long:"crop" description:"Dimensions of the crop region (css shorthand, comma-delimited)"`
+		JPEGQuality    int    `short:"q" long:"quality" description:"JPEG quality (0-100)" default:"85"`
+		Positional     struct {
 			PDF string `positional-arg-name:"<INPUT PDF>"`
 			CBZ string `positional-arg-name:"<OUTPUT CBZ>"`
 		} `positional-args:"yes"`
@@ -37,11 +41,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	// fmt.Println("CROP: " + opts.CropDimensions)
+
 	if !flags.WroteHelp(err) {
 		if opts.Images {
-			err = extractImages(opts.Positional.PDF, opts.Positional.CBZ)
+			err = extractImages(opts.Positional.PDF, opts.Positional.CBZ, opts.CropDimensions)
 		} else {
-			err = extractPages(opts.Positional.PDF, opts.Positional.CBZ, opts.PNG, opts.JPEGQuality)
+			err = extractPages(opts.Positional.PDF, opts.Positional.CBZ, opts.PNG, opts.JPEGQuality, opts.CropDimensions)
 			if err != nil {
 				log.Fatalln(err)
 			}
@@ -49,14 +55,18 @@ func main() {
 	}
 }
 
-func extractPages(pdf,cbz string, usePNG bool, jpegQuality int) error {
+func extractPages(pdf, cbz string, usePNG bool, jpegQuality int, crop string) error {
 	doc, err := fitz.New(pdf)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	defer doc.Close()
 
 	cFile, err := os.Create(cbz)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	zWriter := zip.NewWriter(cFile)
 
@@ -64,7 +74,9 @@ func extractPages(pdf,cbz string, usePNG bool, jpegQuality int) error {
 
 	for n := 0; n < doc.NumPage(); n++ {
 		img, err := doc.Image(n)
-		if err != nil { return err }
+		if err != nil {
+			return err
+		}
 
 		filetype := "jpg"
 		if usePNG {
@@ -73,14 +85,29 @@ func extractPages(pdf,cbz string, usePNG bool, jpegQuality int) error {
 
 		h := &zip.FileHeader{Name: fmt.Sprintf("page%04d.%s", n, filetype), Method: zip.Deflate}
 		f, err := zWriter.CreateHeader(h)
-		if err != nil { return err }
+		if err != nil {
+			return err
+		}
 
-		if usePNG {
-			err = png.Encode(f, img)
-			if err != nil { return err }
+		if crop != "" {
+			simg := cropImage(img, crop)
+			if usePNG {
+				err = png.Encode(f, simg)
+			} else {
+				err = jpeg.Encode(f, simg, &jpeg.Options{Quality: jpegQuality})
+			}
 		} else {
-			err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpegQuality})
-			if err != nil { return err }
+			if usePNG {
+				err = png.Encode(f, img)
+				if err != nil {
+					return err
+				}
+			} else {
+				err = jpeg.Encode(f, img, &jpeg.Options{Quality: jpegQuality})
+				if err != nil {
+					return err
+				}
+			}
 		}
 
 		bar.Add(1)
@@ -92,13 +119,17 @@ func extractPages(pdf,cbz string, usePNG bool, jpegQuality int) error {
 	return nil
 }
 
-func extractImages(pdf,cbz string) error {
+func extractImages(pdf, cbz, crop string) error {
 	cFile, err := os.Create(cbz)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	zWriter := zip.NewWriter(cFile)
 
 	tmp, err := os.MkdirTemp("", "pdf2cbz")
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 
 	err = api.ExtractImagesFile(pdf, tmp, nil, pdfcpu.NewDefaultConfiguration())
 
@@ -106,9 +137,11 @@ func extractImages(pdf,cbz string) error {
 
 	bar := progressbar.New(len(files))
 
-	for _,file := range files {
+	for _, file := range files {
 		f, err := os.Open(tmp + file.Name())
-		if err != nil { return err }
+		if err != nil {
+			return err
+		}
 
 		info, err := file.Info()
 		header, err := zip.FileInfoHeader(info)
@@ -133,4 +166,26 @@ func extractImages(pdf,cbz string) error {
 	cFile.Close()
 
 	return nil
+}
+
+func cropImage(img image.Image, dims string) image.Image {
+	dimsSlice := strSlicetoIntSlice(strings.Split(dims, ","))
+	cropRegion := image.Rect(dimsSlice[0] + img.Bounds().Min.X, dimsSlice[1] + img.Bounds().Min.Y,
+		img.Bounds().Max.X - dimsSlice[2], img.Bounds().Max.Y - dimsSlice[3])
+	crop := img.(interface {
+		SubImage(r image.Rectangle) image.Image
+	}).SubImage(cropRegion)
+
+	// fmt.Printf("ORIGINAL: (%d,%d) x (%d,%d) CROPPED: (%d,%d) x (%d,%d)", img.Bounds().Min.X, img.Bounds().Min.Y, img.Bounds().Max.X, img.Bounds().Max.Y, crop.Bounds().Min.X, crop.Bounds().Min.Y, crop.Bounds().Max.X, crop.Bounds().Max.Y)
+
+	return crop
+}
+
+func strSlicetoIntSlice(strs []string) []int {
+	var ints []int
+	for _,v := range strs {
+		i, _ := strconv.Atoi(v)
+		ints = append(ints, i)
+	}
+	return ints
 }
